b2df1721c4aa253f73b3c388277d29d1c3a8747c
Robert Andersen <rsa@1-0.design>
2025-03-13 14:35:56 -1000

    Add index.js wrapper to support Render.com deployment

---
+++ index.js
@@ -0,0 +1,4 @@
+// This is a simple wrapper to load the actual server file
+// Created specifically for Render.com deployment
+console.log('Starting server from index.js wrapper...');
+require('./server.js');
